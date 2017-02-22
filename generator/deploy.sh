9bb83a673c30d6bc5fdeb5f9f911b040859eb865
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-02-22 18:14:47 +0100

    the last travis's hotfix i expect

--- generator/deploy.sh
+++ generator/deploy.sh
@@ -1,10 +1,7 @@
 cd dist
-ls -al
 git init
-ls -al
-
 git config user.name "MacBootglass"
 git config user.email "thibault.theologien@insa-rouen.fr"
 git add .
 git commit -m "Deploy"
-git push -fq "https://${GH_TOKEN}@${GH_REF}"
+git push -fq --set-upstream "https://${GH_TOKEN}@${GH_REF}" master
