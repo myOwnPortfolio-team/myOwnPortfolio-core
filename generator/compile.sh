1ec775bb138936b6482db49acfb78f65a2353e0b
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-02-23 17:01:19 +0100

    maj travis.yml

--- generator/compile.sh
+++ generator/compile.sh
@@ -1,4 +1,4 @@
 rm -rf ./dist
 mkdir ./dist
-docker build -t macbootglass/env ./generator > /dev/null
-docker run --name=generator --volume=${TRAVIS_BUILD_DIR}:/root/project macbootglass/env > /dev/null
+docker build -t macbootglass/compilator ./generator > /dev/null
+docker run --name=generator --volume=$(pwd):/root/project macbootglass/compilator > /dev/null
