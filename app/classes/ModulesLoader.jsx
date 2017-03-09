6d3e87449593d90df09f22be6a08d1f3db2178e0
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-09 14:30:49 +0100

    generation of dynamical div with error detection in json-schema

--- app/classes/ModulesLoader.jsx
+++ app/classes/ModulesLoader.jsx
@@ -4,13 +4,24 @@ import Ajv from 'ajv';
 const ModulesLoader = React.createClass({
   render: function() {
     let data = require("../config/modules_list.json").modules_list;
-    let schema = require("../config/modules_schemas.json");
-
     let ajv = new Ajv();
-    let valid = ajv.validate(schema, data);
-    if (!valid) console.log(ajv.errors);
+    let valid = ajv.validate(require("../config/modules_schemas.json"), data);
 
-    return (<div>Hello World</div>);
+    if (!valid) {
+      return (
+        <div>
+          <h1>INVALID JSON ENTRY</h1>
+          <span>{JSON.stringify(ajv.errors)}</span> <br/>
+          <span>Check the JSON-Schemas</span>
+        </div>
+      );
+    }
+
+    const modules_list = data.map((module) =>
+      <div key={module.name}>{module.name}</div>
+    );
+
+    return (<div> {modules_list} </div>);
   }
 })
 
--- tools/node/validate-json.js
+++ tools/node/validate-json.js
@@ -1,7 +1,7 @@
 let Ajv = require('ajv');
 
-let data = require("./app/config/modules_list.json").modules_list;
-let schema = require("./app/config/modules_schemas.json");
+let data = require("../../app/config/modules_list.json").modules_list;
+let schema = require("../../app/config/modules_schemas.json");
 
 let ajv = new Ajv();
 let valid = ajv.validate(schema, data);
