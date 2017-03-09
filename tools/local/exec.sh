59d3084b6565b6673a5403cc4557087bb70597d3
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-09 13:51:55 +0100

    ajout serveur express + deplacement fichier validation-json + maj en conséquence dockerfile

--- tools/local/exec.sh
+++ tools/local/exec.sh
@@ -1,1 +1,1 @@
-docker run --name=exec_portfolio --volume=$(pwd):/root/project macbootglass/exec_portfolio
+docker run --name=exec_portfolio --volume=$(pwd):/root/project -p 3000:3000 macbootglass/exec_portfolio
---
+++ tools/node/express.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var opener = require('opener');
+var port = 3000;
+
+app.use("/", express.static('dist'));
+
+app.listen(port, () => {
+  console.log('Server listening on port ' + port + '!')
+});
+
+opener('http://localhost:' + port + '/');
