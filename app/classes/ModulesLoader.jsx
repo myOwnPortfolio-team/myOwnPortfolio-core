b00eaabb36d1131fb708e043d4f0bbda898e2228
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-11 12:48:02 +0100

    update class moduleloader

--- app/classes/ModulesLoader.jsx
+++ app/classes/ModulesLoader.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
-import Ajv from 'ajv';
+var modules = require('../config/import.js').modules_list;
 
-const ModulesLoader = React.createClass({
+module.exports = React.createClass({
   render: function() {
-    let data = require("../config/modules_list.json").modules_list;
-    let ajv = new Ajv();
-    let valid = ajv.validate(require("../config/modules_schemas.json"), data);
-
-    if (!valid) {
-      return (
-        <div>
-          <h1>INVALID JSON ENTRY</h1>
-          <span>{JSON.stringify(ajv.errors)}</span> <br/>
-          <span>Check the JSON-Schemas</span>
-        </div>
-      );
-    }
-
-    const modules_list = data.map((module) =>
-      <div key={module.name}>{module.name}</div>
-    );
+    let modules_list = modules.map((data) => {
+      let Component = data.component;
+      return (<div key={data.name}><Component content={data.content} /></div>);
+    });
 
     return (<div> {modules_list} </div>);
   }
-})
-
-export default ModulesLoader;
+});
