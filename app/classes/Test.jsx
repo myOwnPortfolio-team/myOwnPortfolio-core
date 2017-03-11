e516507b47b57664fb9fdb1b0c1a187d6e2bf8b7
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-11 12:47:47 +0100

    add test class

---
+++ app/classes/Test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+module.exports = React.createClass({
+  render: function() {
+    return(<div>Module de test</div>)
+  }
+});
+//
+// export default Test;
+// class Test extends React.Component {
+//   render() {
+//     return <div>Module de test</div>;
+//   }
+// }
+
+// export default Test;
