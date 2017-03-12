b71db2ea48faa041bbdead187c2f0b50e62cc21c
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-12 16:48:57 +0100

    add properties props for all components potential properties

--- app/classes/ModulesLoader.jsx
+++ app/classes/ModulesLoader.jsx
@@ -5,11 +5,22 @@ const modules = require('../config/import.js').modules_list;
 
 module.exports = React.createClass({
   render: function() {
+    let properties = {
+      "ids" : [],
+    };
     let modules_list = modules.map((data) => {
       let Module = data.module;
+      properties.ids.push("module_"+data.name);
       return (
-        <Component id={"module_" + data.name} key={data.name}>
-          <Module content={data.content} style={data.style}/>
+        <Component
+          id={"module_" + data.name}
+          key={data.name}
+        >
+          <Module
+            content={data.content}
+            style={data.style}
+            properties={properties.ids}
+          />
         </Component>
       );
     });
