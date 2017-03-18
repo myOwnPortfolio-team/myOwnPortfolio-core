a06e83246224982cee03a39d57dc2e18597b29d2
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-18 15:53:35 +0100

    decoupage en fonction de la fonction generatemodulesliste + début de création import.scss

--- tools/node/generate_modules_list.js
+++ tools/node/generate_modules_list.js
@@ -1,56 +1,102 @@
-var fs = require('fs');
-var Ajv = require('ajv');
+const main = function() {
+  const data = require("../../app/config/modules_list.json").modules_list;
+  const schema = require("../../app/config/modules_schema.json");
+  validateJSON(data, schema);
 
-var data = require("../../app/config/modules_list.json").modules_list;
-var ajv = new Ajv();
-var valid = ajv.validate(require("../../app/config/modules_schema.json"), data);
+  var importJS = 'module.exports = { \n\
+    "modules_list": [\n';
+  var importSCSS = '';
 
-if (!valid) {
-  return console.log(JSON.stringify(ajv.errors));
+  for (var i = 0; i < data.length; i++) {
+    importJS += makeJS(data[i], i < data.length - 1);
+    importSCSS += makeSCSS(data[i]);
+  }
+
+  importJS += '\
+    ]\n\
+  }';
+
+  writeFile(importJS, importSCSS);
 }
 
-var content = 'module.exports = { \n\
-  "modules_list": [\n';
 
-for (var i = 0; i < data.length; i++) {
-  content += '\
+const validateJSON = function(jsonData, jsonSchema) {
+  var Ajv = require('ajv');
+  var ajv = new Ajv();
+  var valid = ajv.validate(jsonSchema, jsonData);
+
+  if (!valid) {
+    return console.log(JSON.stringify(ajv.errors));
+  }
+}
+
+
+const makeJS = function(data, isTheLast) {
+  var importJS = '\
     { \n\
-      "name": "' + data[i].name + '", \n\
-      "module": require("' + data[i].module_path + '"), \n';
+      "name": "' + data.name + '", \n\
+      "module": require("' + data.module_path + '"), \n';
 
-  if (data[i].content_path !== undefined && data[i].content_path !== "") {
-    content += '\
-      "content": require("' + data[i].content_path + '"), \n';
+  if (data.content_path !== undefined && data.content_path !== "") {
+    importJS += '\
+      "content": require("' + data.content_path + '"), \n';
   }
   else {
-    content += '\
+    importJS += '\
       "content": "none", \n';
   }
 
-  if (data[i].style_path !== undefined && data[i].style_path !== "") {
-    content += '\
-      "style": require("' + data[i].style_path + '") \n';
+  if (data.style_path !== undefined && data.style_path !== "") {
+    importJS += '\
+      "style": require("' + data.style_path + '") \n';
   }
   else {
-    content += '\
+    importJS += '\
       "style": "none" \n';
   }
 
-  content += '\
+  importJS += '\
     }';
-  if (i < data.length - 1) {
-    content += ',';
+  if (!isTheLast) {
+    importJS += ',';
   }
-  content += '\n';
+  importJS += '\n';
+
+  return importJS;
 }
 
-content += '\
-  ]\n\
-}';
+const makeSCSS = function(data) {
+  if (data.style_path !== undefined && data.style_path !== "") {
+    return '@import "' + data.style_path + '";\n';
+  }
+  else {
+    return '@import "' + makeStylePath(data.module_path) + '";\n'
+  }
+}
 
-fs.writeFile("./app/config/import.js", content, function(err) {
+const writeFile = function(importJS, importSCSS){
+  let fs = require('fs');
+
+  fs.writeFile("./app/config/import.js", importJS, function(err) {
     if(err) {
-        return console.log(err);
+      return console.log(err);
     }
     console.log("Script import.js généré");
-});
+  });
+
+  fs.writeFile("./app/config/import.scss", importSCSS, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log("Fichier import.scss généré");
+  });
+}
+
+const makeStylePath = function(module_path) {
+  var firstPart = "../modules/";
+  var style_path = module_path.substring(firstPart.length);
+  style_path = style_path.substring(0, style_path.indexOf("/"));
+  return firstPart + style_path + "/style/json_config.json";
+}
+
+main();
