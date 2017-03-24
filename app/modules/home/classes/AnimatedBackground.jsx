89e1a9d59d9583e6ebcbac7b56ca64c98550e35e
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-24 16:28:06 +0100

    maj animated background in module home

--- app/modules/home/classes/AnimatedBackground.jsx
+++ app/modules/home/classes/AnimatedBackground.jsx
@@ -4,14 +4,26 @@ import Particles from 'particlesjs';
 module.exports = React.createClass({
   componentDidMount: function() {
     Particles.init({
-       selector: '.background',
-       color: this.props.color,
-     });
+      selector: '.background',
+      color: this.props.color,
+      maxParticles:	this.props.maxParticles,
+      sizeVariations:	this.props.sizeVariations,
+      speed: this.props.speed,
+      minDistance: this.props.minDistance,
+      connectParticles: this.props.connectParticles,
+      responsive: this.props.responsive,
+    });
   },
 
   getDefaultProps: function() {
     return {
       color: '#75A5B7',
+      maxParticles:	100,
+      sizeVariations:	3,
+      speed: 0.5,
+      minDistance: 120,
+      connectParticles: true,
+      responsive: null,
     }
   },
 
