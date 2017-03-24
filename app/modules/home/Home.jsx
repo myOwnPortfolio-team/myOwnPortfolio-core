76b2485383589d0d25e3faf807f8e714e3b6d6c5
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-24 18:24:51 +0100

    add typewriter effect on module home

--- app/modules/home/Home.jsx
+++ app/modules/home/Home.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
 import AnimatedBackground from './classes/AnimatedBackground.jsx';
 import Avatar from './classes/Avatar.jsx';
+import ReactRotatingText from 'react-rotating-text';
+
 
 module.exports = React.createClass({
+  getDefaultProps: function() {
+    return {
+      items: ['unItem'],
+      cursor: true,
+      pause: 1500,
+      emptyPause: 1000,
+      typingInterval: 50,
+      deletingInterval: 50,
+    }
+  },
+
   render: function() {
     return(
       <div
         id={this.props.id}
         className="module_home"
       >
-        <div className="module_home_info">
-          <span className="module_home_title">PORTFOLIO</span>
+        <div className="module_home_content">
           <Avatar src="https://fr.gravatar.com/userimage/45448759/031c14bdffe709c881b0ed4bced8e433.jpg?size=200" />
-          <span className="module_home_username">Thibault THÉOLOGIEN</span>
+          <div className="module_home_typewriter">
+            <span>Vous cherchez</span>
+            <ReactRotatingText
+              items={[' un développeur Web ?', ' un développeur Java ?', ' Thibault THEOLOGIEN !']}
+              cursor={this.props.cursor}
+              pause={this.props.pause}
+              emptyPause={this.props.emptyPause}
+              typingInterval={this.props.typingInterval}
+              deletingInterval={this.props.deletingInterval}
+            />
+          </div>
         </div>
-
         <AnimatedBackground />
       </div>
     );
