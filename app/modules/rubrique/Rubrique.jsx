ce96087d434236acee4075dfe29fcec84ec9d824
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-30 18:51:41 +0200

    generation modulaire de rubriques

--- app/modules/rubrique/Rubrique.jsx
+++ app/modules/rubrique/Rubrique.jsx
@@ -1,54 +1,40 @@
 import React from 'react';
+import Experience from './classes/Experience.js';
+import Education from './classes/Education.js';
+
 
 module.exports = React.createClass({
+  generateExeriences: function(experience) {
+    return experience.map((obj, pos) => {
+      return (
+        <Experience
+          key={"exp_" + pos}
+          content={obj}
+        />
+      )
+    })
+  },
+
+  generateEducation: function(education) {
+    return education.map((obj, pos) => {
+      return (
+        <Education
+          key={"edu_" + pos}
+          content={obj}
+        />
+      )
+    })
+  },
+
   render: function() {
     return (
       <section
         id={this.props.id}
         className="module_rubrique"
       >
-        <h2>Expériences</h2>
-        <article>
-          <p>Poste</p>
-          <p>Entreprise</p>
-          <p>Date - Durée - Emplacement</p>
-          <p>Description</p>
-        </article>
-        <article>
-          <p>Poste</p>
-          <p>Entreprise</p>
-          <p>Date - Durée - Emplacement</p>
-          <p>Description</p>
-        </article>
-        <article>
-          <p>Poste</p>
-          <p>Entreprise</p>
-          <p>Date - Durée - Emplacement</p>
-          <p>Description</p>
-        </article>
-        <article>
-          <p>Poste</p>
-          <p>Entreprise</p>
-          <p>Date - Durée - Emplacement</p>
-          <p>Description</p>
-        </article>
-
-        <h2>Formations</h2>
-        <article>
-          <p>Etablissement</p>
-          <p>Diplôme</p>
-          <p>Date - Emplacement</p>
-        </article>
-        <article>
-          <p>Etablissement</p>
-          <p>Diplôme</p>
-          <p>Date - Emplacement</p>
-        </article>
-        <article>
-          <p>Etablissement</p>
-          <p>Diplôme</p>
-          <p>Date - Emplacement</p>
-        </article>
+        <h2>{this.props.content.title}</h2>
+        {this.generateExeriences(this.props.content.experience)}
+        {this.generateEducation(this.props.content.education)}
       </section>
     );
   }
