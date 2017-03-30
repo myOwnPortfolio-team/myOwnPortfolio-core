0cb79de783c2f4f808d0720811926248fe8a1121
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-03-30 16:53:10 +0200

    ajout dans répertoire doc d'un module vide

---
+++ doc/module_template/Module.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div
+        id={this.props.id}
+      >
+      </div>
+    );
+  }
+});
