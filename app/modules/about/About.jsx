dfe0afef196a94e16deb6924ef3c3ea7ac04d03a
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-04-11 18:44:56 +0200

    ajout module about minimaliste

---
+++ app/modules/about/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <section
+        id={this.props.id}
+      >
+      </section>
+    );
+  }
+});
