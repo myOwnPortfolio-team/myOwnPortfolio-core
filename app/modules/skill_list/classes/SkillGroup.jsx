3e8d2cacbd4d8a4d2862eb4fee93654a91426216
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-06-25 15:36:07 +0100

    HOTFIX - SkillGroup OK

--- app/modules/skill_list/classes/SkillGroup.jsx
+++ app/modules/skill_list/classes/SkillGroup.jsx
@@ -3,19 +3,21 @@ import Visible from 'react-on-visible';
 import Skill from './Skill.jsx';
 const slug = require('slug');
 
-module.exports = React.createClass({
-  componentDidMount: function() {
-    this.tm = null;
-  },
 
-  getInitialState: function() {
-    return {
+class SkillGroup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       visible: false,
       percent: 0,
-    }
-  },
+    };
+  }
 
-  increase: function() {
+  componentDidMount() {
+    this.tm = null;
+  }
+
+  increase() {
     let percent = this.state.percent + 1;
     let max = 100;
     if (percent > max) {
@@ -24,8 +26,8 @@ module.exports = React.createClass({
       return;
     }
     this.setState({ percent });
-    this.tm = setTimeout(this.increase, 10);
-  },
+    this.tm = setTimeout(() => this.increase(), 10);
+  }
 
   restart(visible) {
     if (this.state.visible !== visible && visible === true) {
@@ -47,9 +49,9 @@ module.exports = React.createClass({
         visible: visible
       });
     }
-  },
+  }
 
-  generateSkills: function(skills) {
+  generateSkills(skills) {
     return skills.map((obj, pos) => {
       let key = slug("skill " + obj.name + " " + pos, {lower: true, replacement: "_"});
       return (
@@ -60,12 +62,12 @@ module.exports = React.createClass({
         />
       )
     });
-  },
+  }
 
-  render: function() {
+  render() {
     return (
       <Visible
-        onChange={this.restart}
+        onChange={(visible) => this.restart(visible)}
         bounce={true}
         percent={50}
       >
@@ -79,93 +81,6 @@ module.exports = React.createClass({
       </Visible>
     );
   }
-});
+}
 
-
-
-// import React from 'react';
-// import Visible from 'react-on-visible';
-// import Skill from './Skill.jsx';
-// const slug = require('slug');
-//
-// class SkillGroup extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       visible: false,
-//       percent: 0,
-//     };
-//   }
-//
-//   componentDidMount() {
-//     this.tm = null;
-//     console.log(this.state);
-//   }
-//
-//   increase() {
-//     let percent = this.state.percent + 1;
-//     let max = 100;
-//     if (percent > max) {
-//       percent = max;
-//       clearTimeout(this.tm);
-//       return;
-//     }
-//     this.setState({ percent });
-//     this.tm = setTimeout(this.increase, 10);
-//   }
-//
-//   restart(visible) {
-//     if (this.state.visible !== visible && visible === true) {
-//       clearTimeout(this.tm);
-//       this.setState(
-//         {
-//           percent: 0,
-//           visible: visible
-//         },
-//         () => {
-//           this.increase();
-//         }
-//       );
-//     }
-//
-//     if (this.state.visible !== visible && visible === false) {
-//       this.setState({
-//         percent: 0,
-//         visible: visible
-//       });
-//     }
-//   }
-//
-//   generateSkills(skills) {
-//     return skills.map((obj, pos) => {
-//       let key = slug("skill " + obj.name + " " + pos, {lower: true, replacement: "_"});
-//       return (
-//         <Skill
-//           key={key}
-//           content={obj}
-//           percent={this.state.percent}
-//         />
-//       )
-//     });
-//   }
-//
-//   render() {
-//     return (
-//       <Visible
-//         onChange={this.restart}
-//         bounce={true}
-//         percent={50}
-//       >
-//         <div
-//           className="module_skill_list_group"
-//           data-aos="flip-left"
-//         >
-//           <h3>{this.props.content.name}</h3>
-//           {this.generateSkills(this.props.content.skills)}
-//         </div>
-//       </Visible>
-//     );
-//   }
-// }
-//
-// module.exports = SkillGroup;
+module.exports = SkillGroup;
