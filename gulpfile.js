7b474a2496515fea7719a7d08c9685b6116462e8
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-09-27 19:33:24 +0200

    HOTFIX - gulpfile avec eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,48 @@
-var path = require('path');
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
+const path = require('path');
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
 
 plugins.exec = require('gulp-exec');
 plugins.sass = require('gulp-ruby-sass');
 plugins.webpack = require('webpack-stream');
 
 // var webpack = require('webpack');
-var src = './app'
-var dest = './dist';
+const src = './app';
+const dest = './dist';
 
-var execOptions = {
+const execOptions = {
   continueOnError: false, // default = false, true means don't emit error event
   pipeStdout: false, // default = false, true means stdout is written to file.contents
-  customTemplatingThing: "test" // content passed to gutil.template()
+  customTemplatingThing: 'test', // content passed to gutil.template()
 };
 
-var execReportOptions = {
+const execReportOptions = {
   err: true, // default = true, false means don't write err
   stderr: true, // default = true, false means don't write stderr
-  stdout: true // default = true, false means don't write stdout
+  stdout: true, // default = true, false means don't write stdout
 };
 
-var configWebpack = {
+const configWebpack = {
   output: {
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
-        test : /\.jsx?$/,
-        include : path.resolve(__dirname, 'app'),
-        loader : 'babel'
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'app'),
+        loader: 'babel',
       },
       {
         test: /\.json$/,
-        loader: "json"
-      }
-    ]
+        loader: 'json',
+      },
+    ],
   },
   resolve: {
     alias: {
-      "react": __dirname + '/node_modules/react',
-    }
+      react: path.join(__dirname, '/node_modules/react'),
+    },
   },
   // plugins: [
   //   new webpack.DefinePlugin({
@@ -54,101 +54,78 @@ var configWebpack = {
   // ]
 };
 
-gulp.task('documentation', function() {
-  return gulp.src('./tools/node/generate_doc.js')
-    .pipe(plugins.exec('node <%= file.path %>', execOptions))
-    .pipe(plugins.exec.reporter(execReportOptions))
-    .pipe(plugins.exec('browserify ./docs/json_schema/script.js -o ./docs/json_schema/bundle.js', execOptions))
-    .pipe(plugins.exec.reporter(execReportOptions))
-});
+gulp.task('documentation', () => gulp.src('./tools/node/generate_doc.js')
+  .pipe(plugins.exec('node <%= file.path %>', execOptions))
+  .pipe(plugins.exec.reporter(execReportOptions))
+  .pipe(plugins.exec('browserify ./docs/json_schema/script.js -o ./docs/json_schema/bundle.js', execOptions))
+  .pipe(plugins.exec.reporter(execReportOptions)));
 
-gulp.task('copyHTML', function() {
-  return gulp.src(src + '/index.html')
+gulp.task('copyHTML', () => gulp.src(`${src}/index.html`)
   .pipe(gulp.dest(dest))
-  .pipe(plugins.livereload());
-});
+  .pipe(plugins.livereload()));
 
-gulp.task('copyFonts', function() {
-  return gulp.src('./node_modules/font-awesome/fonts/*')
-  .pipe(gulp.dest(dest + "/fonts"))
-  .pipe(plugins.livereload());
-});
+gulp.task('copyFonts', () => gulp.src('./node_modules/font-awesome/fonts/*')
+  .pipe(gulp.dest(`${dest}/fonts`))
+  .pipe(plugins.livereload()));
 
-gulp.task('webpack', function() {
-  return gulp.src(src + '/index.jsx')
+gulp.task('webpack', () => gulp.src(`${src}/index.jsx`)
   .pipe(plugins.webpack(configWebpack))
-  .pipe(gulp.dest(dest + '/script'))
-  .pipe(plugins.livereload());
-});
+  .pipe(gulp.dest(`${dest}/script`))
+  .pipe(plugins.livereload()));
 
-gulp.task('compileCSS', () =>
-    plugins.sass(src + '/index.scss', {
-      sourcemap: false,
-      require: ["sass-json-vars"]
-    })
-    .on('error', plugins.sass.logError)
-    .pipe(plugins.csscomb())
-    .pipe(plugins.cssbeautify({indent: '  '}))
-    .pipe(plugins.autoprefixer())
-    .pipe(gulp.dest(dest + '/style/'))
-    .pipe(plugins.livereload())
+gulp.task('compileCSS', () => plugins.sass(`${src}/index.scss`, {
+  sourcemap: false,
+  require: ['sass-json-vars'],
+}).on('error', plugins.sass.logError)
+  .pipe(plugins.csscomb())
+  .pipe(plugins.cssbeautify({ indent: '  ' }))
+  .pipe(plugins.autoprefixer())
+  .pipe(gulp.dest(`${dest}/style/`))
+  .pipe(plugins.livereload()),
 );
 
-gulp.task('minifyCSS', function () {
-  return gulp.src(dest + '/style/index.css')
-    .pipe(plugins.csso())
-    .pipe(plugins.rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(dest + '/style/'));
-});
+gulp.task('minifyCSS', () => gulp.src(`${dest}/style/index.css`)
+  .pipe(plugins.csso())
+  .pipe(plugins.rename({
+    suffix: '.min',
+  }))
+  .pipe(gulp.dest(`${dest}/style/`)));
 
-gulp.task('minifyJS', function() {
-  return gulp.src(dest + '/script/bundle.js')
-    .pipe(plugins.uglify())
-    .pipe(plugins.rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(dest + '/script/'));
-});
+gulp.task('minifyJS', () => gulp.src(`${dest}/script/bundle.js`)
+  .pipe(plugins.uglify())
+  .pipe(plugins.rename({
+    suffix: '.min',
+  }))
+  .pipe(gulp.dest(`${dest}/script/`)));
 
-gulp.task('generateModulesList', function() {
-  return gulp.src('./tools/node/generate_modules_list.js')
-    .pipe(plugins.exec('node <%= file.path %>', execOptions))
-    .pipe(plugins.exec.reporter(execReportOptions))
-    .pipe(plugins.livereload());
-});
+gulp.task('generateModulesList', () => gulp.src('./tools/node/generate_modules_list.js')
+  .pipe(plugins.exec('node <%= file.path %>', execOptions))
+  .pipe(plugins.exec.reporter(execReportOptions))
+  .pipe(plugins.livereload()));
 
-gulp.task('serverStart', function() {
-  return gulp.src('./tools/node/express.js')
-    .pipe(plugins.exec('node <%= file.path %>', execOptions))
-    .pipe(plugins.exec.reporter(execReportOptions));
-});
+gulp.task('serverStart', () => gulp.src('./tools/node/express.js')
+  .pipe(plugins.exec('node <%= file.path %>', execOptions))
+  .pipe(plugins.exec.reporter(execReportOptions)));
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
   plugins.livereload.listen();
-
-  gulp.watch(src + '/style/*/*.scss', ['compileCSS']);
-  gulp.watch(src + '/style/*.scss', ['compileCSS']);
-  gulp.watch(src + '/modules/*/style.scss', ['compileCSS']);
-  gulp.watch(src + '/modules/*/json_config/style.json', ['compileCSS']);
-
-  gulp.watch(src + '/modules/*/*/*.jsx', ['webpack']);
-  gulp.watch(src + '/modules/*/*.jsx', ['webpack']);
-  gulp.watch(src + '/classes/*.jsx', ['webpack']);
-  gulp.watch(src + '/*.jsx', ['webpack']);
-
-  gulp.watch(src + '/config/*.json',  plugins.sequence('generateModulesList', 'webpack'));
-  gulp.watch(src + '/config/*/*.json',  ['webpack']);
-  gulp.watch(src + '/modules/*/*.json',  ['webpack', 'compileCSS']);
-
+  gulp.watch(`${src}/style/*/*.scss`, ['compileCSS']);
+  gulp.watch(`${src}/style/*.scss`, ['compileCSS']);
+  gulp.watch(`${src}/modules/*/style.scss`, ['compileCSS']);
+  gulp.watch(`${src}/modules/*/json_config/style.json`, ['compileCSS']);
+  gulp.watch(`${src}/modules/*/*/*.jsx`, ['webpack']);
+  gulp.watch(`${src}/modules/*/*.jsx`, ['webpack']);
+  gulp.watch(`${src}/classes/*.jsx`, ['webpack']);
+  gulp.watch(`${src}/*.jsx`, ['webpack']);
+  gulp.watch(`${src}/config/*.json`, plugins.sequence('generateModulesList', 'webpack'));
+  gulp.watch(`${src}/config/*/*.json`, ['webpack']);
+  gulp.watch(`${src}/modules/*/*.json`, ['webpack', 'compileCSS']);
+  gulp.watch(`${src}/index.html`, ['copyHTML']);
   gulp.watch('./tools/node/*.js', ['generateModulesList']);
-
-  gulp.watch(src + '/index.html',  ['copyHTML']);
 });
 
 gulp.task('build', plugins.sequence('generateModulesList', 'webpack', ['compileCSS', 'copyHTML', 'copyFonts']));
 gulp.task('dev', ['build', 'serverStart', 'watch']);
 gulp.task('minify', ['minifyCSS', 'minifyJS']);
-gulp.task('prod', plugins.sequence('build',  'minify'));
+gulp.task('prod', plugins.sequence('build', 'minify'));
 gulp.task('default', ['build']);
