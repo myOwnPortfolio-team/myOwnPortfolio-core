503d7f78423b2325909f827a56f48ec84aaa9af4
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-09-27 17:32:18 +0200

    UPDATE - supression de la configuration travis dorénavant inutile -> mise à jour du forké par la suite

--- tools/travis/compile.sh
+++
@@ -1,3 +0,0 @@
-rm -rf ./dist
-mkdir ./dist
-docker run --name=compile_myownportfolio --volume=$(pwd):/root/project -it macbootglass/myownportfolio bash compile.sh > /dev/null
--- tools/travis/deploy.sh
+++
@@ -1,7 +0,0 @@
-cd dist
-git init
-git config user.name "MacBootglass"
-git config user.email "thibault.theologien@insa-rouen.fr"
-git add .
-git commit -m "Deployed with Travis and from myOwnPortfolio"
-git push -fq --set-upstream "https://${GH_TOKEN}@${GH_REF}" master
