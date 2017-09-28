067a9e37137ebfd4e7827b88a304539390e25054
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-09-28 18:10:16 +0200

    HOTFIX - eslint on about and index

--- app/modules/about/About.jsx
+++ app/modules/about/About.jsx
@@ -1,21 +1,25 @@
+/* global window */
+
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import PropTypes from 'prop-types';
 import LogoLink from './classes/LogoLink.jsx';
+
 const slug = require('slug');
 
-class About extends React.Component {
-  generate_logos_links(links) {
-    return links.map((obj, pos) => {
-      let key = slug("link to " + obj.alt, {lower: true, replacement: "_"});
-      return (
-        <LogoLink
-          key={key}
-          properties={obj}
-        />
-      )
-    });
-  }
+function generateLogosLinks(links) {
+  return links.map((obj) => {
+    const key = slug(`link to ${obj.alt}`, { lower: true, replacement: '_' });
+    return (
+      <LogoLink
+        key={key}
+        properties={obj}
+      />
+    );
+  });
+}
 
+class About extends React.Component {
   render() {
     return (
       <section
@@ -35,7 +39,7 @@ class About extends React.Component {
           data-aos={this.props.properties.content_animation}
         >
           <ReactMarkdown
-            source={this.props.content.text.join(" \n")}
+            source={this.props.content.text.join(' \n')}
           />
         </div>
 
@@ -43,12 +47,12 @@ class About extends React.Component {
           className="module_about_links"
           data-aos={this.props.properties.content_animation}
         >
-          {this.generate_logos_links(this.props.properties.links)}
+          {generateLogosLinks(this.props.properties.links)}
         </div>
 
         <button
           className="btn module_about_download"
-          onClick={() => { window.open(this.props.properties.resume_link);}}
+          onClick={() => window.open(this.props.properties.resume_link)}
         >
           {this.props.content.download}
         </button>
@@ -56,5 +60,20 @@ class About extends React.Component {
     );
   }
 }
+
+About.propTypes = {
+  id: PropTypes.string.isRequired,
+  properties: PropTypes.shape({
+    module_animation: PropTypes.string,
+    content_animation: PropTypes.string,
+    links: PropTypes.array,
+    resume_link: PropTypes.string,
+  }).isRequired,
+  content: PropTypes.shape({
+    title: PropTypes.string,
+    text: PropTypes.array,
+    download: PropTypes.string,
+  }).isRequired,
+};
 
 module.exports = About;
