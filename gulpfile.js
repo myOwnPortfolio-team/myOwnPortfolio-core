5be9d7de17cde5067f22f1fb0a1dffde765065bc
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-09-28 17:41:44 +0200

    UPDATE - gulpfile -> lighter

--- gulpfile.js
+++ gulpfile.js
@@ -6,37 +6,20 @@ plugins.sass = require('gulp-ruby-sass');
 plugins.webpack = require('webpack-stream');
 
 const src = './app';
-const dest = './dist';
-
-const execOptions = {
-  continueOnError: false, // default = false, true means don't emit error event
-  pipeStdout: false, // default = false, true means stdout is written to file.contents
-  customTemplatingThing: 'test', // content passed to gutil.template()
-};
-
-const execReportOptions = {
-  err: true, // default = true, false means don't write err
-  stderr: true, // default = true, false means don't write stderr
-  stdout: true, // default = true, false means don't write stdout
-};
 
 gulp.task('webpack', () => gulp.src(`${src}/index.jsx`)
-  .pipe(plugins.exec('npm run build', execOptions))
-  .pipe(plugins.exec.reporter(execReportOptions))
+  .pipe(plugins.exec('npm run build'))
+  .pipe(plugins.exec.reporter())
   .pipe(plugins.livereload()));
 
-gulp.task('compileCSS', () => plugins.sass(`${src}/index.scss`, {
-  sourcemap: false,
-  require: ['sass-json-vars'],
-}).on('error', plugins.sass.logError)
-  .pipe(plugins.csso())
-  .pipe(gulp.dest(`${dest}/style/`))
-  .pipe(plugins.livereload()),
-);
+gulp.task('compileCSS', () => gulp.src(`${src}/index.scss`)
+  .pipe(plugins.exec('npm run sass'))
+  .pipe(plugins.exec.reporter())
+  .pipe(plugins.livereload()));
 
 gulp.task('generateModulesList', () => gulp.src('./etc/scripts/generate_modules_list.js')
-  .pipe(plugins.exec('npm run compile', execOptions))
-  .pipe(plugins.exec.reporter(execReportOptions))
+  .pipe(plugins.exec('npm run compile'))
+  .pipe(plugins.exec.reporter())
   .pipe(plugins.livereload()));
 
 gulp.task('watch', () => {
