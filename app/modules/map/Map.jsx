081e58daee53b64bbb6330da0ac8b6be445ad13c
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-10-04 15:30:57 +0200

    HOTFIX - eslint Navbar

--- app/modules/map/Map.jsx
+++ app/modules/map/Map.jsx
@@ -1,23 +1,25 @@
+/* global window */
+
 import React from 'react';
-var Map = require('react-d3-map').Map;
+
+const D3Map = require('react-d3-map').Map;
 
 class Map extends React.Component {
   render() {
-    var width = window.screen.width;
+    const width = window.screen.width;
 
     return (
       <section
         id={this.props.id}
         className="module_map"
       >
-        <Map
-          width= {width}
+        <D3Map
+          width={width}
           height={width * 0.5}
           scale={1200}
-          scaleExtent= {[1 << 12, 1 << 13]}
-          center= {[-5, 55.4]}
-        >
-        </Map>
+          scaleExtent={[1 << 12, 1 << 13]}
+          center={[-5, 55.4]}
+        />
       </section>
     );
   }
--- app/modules/navbar/Navbar.jsx
+++ app/modules/navbar/Navbar.jsx
@@ -1,58 +1,57 @@
+/* global document */
+
 import React from 'react';
 import Headroom from 'headroom.js';
 
-const RANDOM_ID = "random_id_" + parseInt(Math.random() * 10000);
+const RANDOM_ID = `random_id_${parseInt(Math.random() * 10000, 10)}`;
+
+function isNameUnreferenced(unreferencedName, currentName) {
+  for (let i = 0; i < unreferencedName.length; i++) {
+    if (unreferencedName[i] === currentName) return true;
+  }
+  return false;
+}
 
 class Navbar extends React.Component {
   componentDidMount() {
-    var headroom = new Headroom(document.getElementById(RANDOM_ID));
+    const headroom = new Headroom(document.getElementById(RANDOM_ID));
     headroom.init();
   }
 
   generateLinks(links) {
     let i = 0;
     return links.map((id, pos) => {
-      if (id !== this.props.id && !this.isNameUnreferenced(this.props.links.name_unreferenced, this.props.links.name_list[pos])) {
-        var icon = this.props.properties.icons_list[i++];
-        if (icon === undefined) {
-          icon = "fa-question";
-        }
+      if (id !== this.props.id && !isNameUnreferenced(
+        this.props.links.name_unreferenced, this.props.links.name_list[pos])) {
+        let icon = this.props.properties.icons_list[i++];
+        if (icon === undefined) icon = 'fa-question';
         return (
           <a
-            key={"link_to_" + id}
+            key={`link_to_${id}`}
             className="module_navbar_link"
-            href={"#"+id}
+            href={`#${id}`}
           >
             <span className="module_navbar_link_text">
               {this.props.links.name_list[pos]}
             </span>
             <i
-              className={"fa fa-2x module_navbar_link_icon " + icon}
+              className={`fa fa-2x module_navbar_link_icon ${icon}`}
               aria-hidden="true"
-              onClick={function() {console.log("click");}}
             />
           </a>
         );
       }
+      return null;
     });
-  }
-
-  isNameUnreferenced(unreferencedName, currentName) {
-    for (var i = 0; i < unreferencedName.length; i++) {
-      if (unreferencedName[i] === currentName) {
-        return true;
-      }
-    }
-    return false;
   }
 
   render() {
     return (
-        <nav id={RANDOM_ID} className="navbar navbar-light bg-faded module_navbar">
-          <div className="module_navbar_link_group">
-            {this.generateLinks(this.props.links.id_list)}
-          </div>
-        </nav>
+      <nav id={RANDOM_ID} className="navbar navbar-light bg-faded module_navbar">
+        <div className="module_navbar_link_group">
+          {this.generateLinks(this.props.links.id_list)}
+        </div>
+      </nav>
     );
   }
 }
