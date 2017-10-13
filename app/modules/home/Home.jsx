f5220402705c8507a8baa1b5092f12d5ff2cc101
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-10-13 16:24:12 +0200

    HOTFIX - home module in new react syntaxe

--- app/modules/home/Home.jsx
+++ app/modules/home/Home.jsx
@@ -6,37 +6,34 @@ import ReactRotatingText from 'react-rotating-text';
 import AnimatedBackground from './classes/AnimatedBackground.jsx';
 import Avatar from './classes/Avatar.jsx';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div
-        id={this.props.id}
-        className="module-home"
-      >
-        <div className="module-home-content">
-          <Avatar properties={this.props.properties.avatar} />
-          <div className="module-home-typewriter">
-            <span>{this.props.content.before_typewriter}</span>
-            <ReactRotatingText
-              items={this.props.properties.typewriter.items}
-              cursor={this.props.properties.typewriter.cursor}
-              pause={this.props.properties.typewriter.pause}
-              emptyPause={this.props.properties.typewriter.emptyPause}
-              typingInterval={this.props.properties.typewriter.typingInterval}
-              deletingInterval={this.props.properties.typewriter.deletingInterval}
-            />
-          </div>
-          <button
-            className="module-home-button btn btn-default"
-            onClick={() => window.scrollTo(0, window.innerHeight)}
-          >
-            <i className="fa fa-chevron-down" aria-hidden="true" />
-          </button>
-        </div>
-        <AnimatedBackground properties={this.props.properties.background} />
+const Home = props => (
+  <div
+    id={props.id}
+    className="module-home"
+  >
+    <div className="module-home-content">
+      <Avatar properties={props.properties.avatar} />
+      <div className="module-home-typewriter">
+        <span>{props.content.before_typewriter}</span>
+        <ReactRotatingText
+          items={props.properties.typewriter.items}
+          cursor={props.properties.typewriter.cursor}
+          pause={props.properties.typewriter.pause}
+          emptyPause={props.properties.typewriter.emptyPause}
+          typingInterval={props.properties.typewriter.typingInterval}
+          deletingInterval={props.properties.typewriter.deletingInterval}
+        />
       </div>
-    );
-  }
-}
+      <button
+        className="module-home-button btn btn-default"
+        onClick={() => window.scrollTo(0, window.innerHeight)}
+      >
+        <i className="fa fa-chevron-down" aria-hidden="true" />
+      </button>
+    </div>
+    <AnimatedBackground properties={props.properties.background} />
+  </div>
+);
+
 
 module.exports = Home;
--- app/modules/home/classes/Avatar.jsx
+++ app/modules/home/classes/Avatar.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-class Avatar extends React.Component {
-  render() {
-    return (
-      <img
-        src={this.props.properties.src}
-        alt={this.props.properties.alt}
-        className="module-home-profile-picture"
-      />
-    );
-  }
-}
+const Avatar = props => (
+  <img
+    src={props.properties.src}
+    alt={props.properties.alt}
+    className="module-home-profile-picture"
+  />
+);
 
 module.exports = Avatar;
