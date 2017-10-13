974c609d9428296d8f5701ec49f9bc6988764541
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-10-13 16:21:45 +0200

    HOTFIX - footer module in new react syntaxe

--- app/modules/footer/Footer.jsx
+++ app/modules/footer/Footer.jsx
@@ -3,34 +3,31 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer
-        id={this.props.id}
-        className="module-footer"
+const Footer = props => (
+  <footer
+    id={props.id}
+    className="module-footer"
+  >
+    <ReactMarkdown
+      className="module-footer-content-left"
+      source={props.content.content_left}
+    />
+    <div
+      className="module-footer-center"
+    >
+      <button
+        className="btn"
+        onClick={() => window.scrollTo(0, 0)}
       >
-        <ReactMarkdown
-          className="module-footer-content-left"
-          source={this.props.content.content_left}
-        />
-        <div
-          className="module-footer-center"
-        >
-          <button
-            className="btn"
-            onClick={() => window.scrollTo(0, 0)}
-          >
-            <i className="fa fa-chevron-up" aria-hidden="true" />
-          </button>
-        </div>
-        <ReactMarkdown
-          className="module-footer-content-right"
-          source={this.props.content.content_right}
-        />
-      </footer>
-    );
-  }
-}
+        <i className="fa fa-chevron-up" aria-hidden="true" />
+      </button>
+    </div>
+    <ReactMarkdown
+      className="module-footer-content-right"
+      source={props.content.content_right}
+    />
+  </footer>
+);
+
 
 module.exports = Footer;
