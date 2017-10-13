670071e3b61027f3a5b6218c0c928c98b5e3e413
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-10-13 16:27:12 +0200

    HOTFIX - map module in new react syntaxe

--- app/modules/map/Map.jsx
+++ app/modules/map/Map.jsx
@@ -4,25 +4,22 @@ import React from 'react';
 
 const D3Map = require('react-d3-map').Map;
 
-class Map extends React.Component {
-  render() {
-    const width = window.screen.width;
-
-    return (
-      <section
-        id={this.props.id}
-        className="module_map"
-      >
-        <D3Map
-          width={width}
-          height={width * 0.5}
-          scale={1200}
-          scaleExtent={[1 << 12, 1 << 13]}
-          center={[-5, 55.4]}
-        />
-      </section>
-    );
-  }
-}
+const Map = (props) => {
+  const width = window.screen.width;
+  return (
+    <section
+      id={props.id}
+      className="module_map"
+    >
+      <D3Map
+        width={width}
+        height={width * 0.5}
+        scale={1200}
+        scaleExtent={[1 << 12, 1 << 13]}
+        center={[-5, 55.4]}
+      />
+    </section>
+  );
+};
 
 module.exports = Map;
