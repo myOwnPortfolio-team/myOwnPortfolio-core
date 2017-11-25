c66dc9be0edff07be844ab9d9636cb08fbdca467
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-11-25 13:06:39 +0100

    HOTFIX - react syntax in ModuleLoader

--- app/classes/ModulesLoader.jsx
+++ app/classes/ModulesLoader.jsx
@@ -5,43 +5,41 @@ const slug = require('slug');
 const modules = require('../import.js').modules_list;
 const properties = require('../import.js').properties;
 
-class ModuleLoader extends React.Component {
-  render() {
-    const links = {
-      id_list: [],
-      name_list: [],
-      name_unreferenced: [],
-    };
-    const modulesList = modules.map((data) => {
-      const name = slug(`module ${data.name}`, { lower: true, replacement: '_' });
-      const Module = data.module;
-      links.id_list.push(name);
+const links = {
+  id_list: [],
+  name_list: [],
+  name_unreferenced: [],
+};
 
-      links.name_list.push(data.name);
-      if (!data.referenced) {
-        links.name_unreferenced.push(data.name);
-      }
+const modulesList = modules.map((data) => {
+  const name = slug(`module ${data.name}`, { lower: true, replacement: '_' });
+  const Module = data.module;
+  links.id_list.push(name);
 
-      return (
-        <Module
-          id={name}
-          key={name}
-          content={data.content}
-          properties={data.properties}
-          links={links}
-        />
-      );
-    });
-    return (
-      <div className="module-loader">
-        <Helmet>
-          <title>{properties.title}</title>
-          <link rel="icon" href="" />
-        </Helmet>
-        { modulesList }
-      </div>
-    );
+  links.name_list.push(data.name);
+  if (!data.referenced) {
+    links.name_unreferenced.push(data.name);
   }
-}
+
+  return (
+    <Module
+      id={name}
+      key={name}
+      content={data.content}
+      properties={data.properties}
+      links={links}
+    />
+  );
+});
+
+const ModuleLoader = () => (
+  <div className="module-loader">
+    <Helmet>
+      <title>{properties.title}</title>
+      <link rel="icon" href="" />
+    </Helmet>
+    { modulesList }
+  </div>
+);
 
 module.exports = ModuleLoader;
